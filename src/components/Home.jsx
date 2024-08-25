40d451dd2399caf15d187aa0ea552b9239e891aa
Sundaram-rathor <121678006+Sundaram-rathor@users.noreply.github.com>
2024-08-26 00:35:40 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Maxwidthwrapper from './Maxwidthwrapper'
+
+function Home() {
+  return (
+    <div>
+    <div className='h-[15vh] w-full bg-[#C5705D] flex items-center justify-center rounded-b-lg shadow-2xl'>
+      
+        
+           <div className='flex items-center justify-center'>
+              <div className='text-5xl text-black font-bold tracking-wider hover:tracking-widest duration-700'>
+                Indian Sign Language(ISL) detecter
+              </div>
+           </div>
+        
+      
+
+    </div>
+
+    <div>
+      fdlkjs
+    </div>
+
+    </div>
+
+
+
+      
+   
+  )
+}
+
+export default Home
